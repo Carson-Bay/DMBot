96adbe7ebdcb3203d24b8d0cce379734d1544482
Carson-Bay <cgbman02@gmail.com>
2021-02-19 22:32:37 -0500

    Main.py and command handler

---
+++ Bot/main.py
@@ -0,0 +1,35 @@
+import os
+import discord
+from dotenv import load_dotenv
+from commands import *
+
+# Dictionary of commands
+commands = {}
+
+# Change to check what prefix is stored in server object <--------------------
+prefix = "!"
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(ctx):
+    if ctx.author == ctx.user:
+        return
+
+    if ctx.content.startswith(prefix):
+        command_string = ctx.content.split(" ")[0][len(prefix):].lower()
+
+        if command_string in commands:
+            await commands[command_string](ctx, client)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    TOKEN = os.getenv("BOT_TOKEN")
+    client.run("ODEyNTE5MzcwODY5NTcxNjA1.YDB7oQ.1wQamKSmDJYDsHpKxS8uPNSf-ZQ")
