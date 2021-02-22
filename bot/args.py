225154f97a0979cedeafa631f9faf82cd2b82bce
Kurtis Knodel <kurtisknodel@gmail.com>
2021-02-21 22:37:09 -0800

    Improved argument parser

--- bot/args.py
+++ bot/args.py
@@ -1,13 +1,42 @@
 import re
 
+# This is a fairly liberal parser. Any errors like missing quotes will mostly be ignored.
 def parse_args(args):
-	parsed = re.split('\s+(?=(?:[^\'"]*[\'"][^\'"]*[\'"])*[^\'"]*$)', args)
-	for i in range(0, len(parsed)):
-		parsed[i] = parsed[i].replace('"', '').replace("'", "")
+	parsed = []
+	index = 0
+	inner = False
+	while index < len(args):
+		prev = index
+		index = args.find('"', index + 1)
+
+		chunk = args[prev:index].replace('"', '')
+		if inner:
+			parsed.append(chunk)
+		else:
+			split = re.split("\s", chunk.strip())
+			for s in split:
+				parsed.append(s)
+		
+		inner = not inner
+		if index < 0:
+			break
+
 	return parsed
 
 if __name__ == "__main__":
 	# TEST
 	s = 'This is a "test. The stuff in quotes" should not get split.'
+	s = parse_args(s)
+	print(s)
+
+	s = 'This is another "test. It should probably result in some error.'
+	s = parse_args(s)
+	print(s)
+
+	s = 'This should also error!"'
+	s = parse_args(s)
+	print(s)
+
+	s = 'And yet another " error.'
 	s = parse_args(s)
 	print(s)
