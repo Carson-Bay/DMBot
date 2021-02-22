cef45429f393f6a1878dd37bba2dd70b067397b0
Kurtis Knodel <kurtisknodel@gmail.com>
2021-02-21 20:39:47 -0800

    Wiped old bot code

--- Bot/commands/ping.py
+++
@@ -1,6 +0,0 @@
-import discord
-from Bot.commands.embedMessage import *
-
-
-async def ping(ctx: discord.Message, client: discord.Client):
-    await ctx.channel.send(embed=create("Pong", "", "green"))
