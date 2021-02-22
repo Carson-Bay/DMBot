5bf68b8d07071c398d2b62687b7ecf57e91f467a
Kurtis Knodel <kurtisknodel@gmail.com>
2021-02-21 21:52:04 -0800

    Fixed framework bugs (because I'm bad at Python)

--- bot/main.py
+++ bot/main.py
@@ -2,38 +2,37 @@ import discord
 import os
 from dotenv import load_dotenv
 
-from bot import commands
-from bot.state import State
-from bot.args import parse_args
+import commands
+from state import State
+from args import parse_args
 
 COMMANDS = {
 	"echo": commands.echo
 }
 
+client = discord.Client()
 state = None
 
 def main():
-	load_dotenv() # Loads the .env file. Vars can be retrieved via os.getenv("NAME")
-	client = discord.Client()
-	token = os.getenv("TOKEN")
-	client.run(token)
+	global state
 
+	load_dotenv() # Loads the .env file. Vars can be retrieved via os.getenv("NAME")
+	token = os.getenv("TOKEN")
 	state = State(client)
+	client.run(token)
 
 @client.event
 async def on_ready():
-	client = state.client
 	print("Logged in as {0}".format(client.user))
 
 @client.event
 async def on_message(message):
-	client = state.client
 	if message.author == client.user:
 		return # Don't respond to messages sent by ourselves
 
 	content = message.content
 	prefix = state.get_prefix(message.guild)
-	if not content.starts_with(prefix):
+	if not content.startswith(prefix):
 		return # Make sure the prefix matches
 
 	args = parse_args(message.content)
