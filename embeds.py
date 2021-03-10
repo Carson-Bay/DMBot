1a71622d05f21ec6b194f8dce5e94f4924e078b6
Kurtis Knodel <kurtisknodel@gmail.com>
2021-03-09 16:31:32 -0800

    Updated colours to be discord colours

--- embeds.py
+++ embeds.py
@@ -1,12 +1,12 @@
-from discord import Embed
+from discord import Embed, Colour
 
 COLOURS = {
-	"red": 0xff0000,
-	"orange": 0xffa500,
-	"yellow": 0xffff00,
-	"green": 0x008000,
-	"blue": 0x0000ff,
-	"purple": 0xab00ff
+	"red": Colour.red(),
+	"orange": Colour.orange(),
+	"yellow": Colour.gold(),
+	"green": Colour.green(),
+	"blue": Colour.blue(),
+	"purple": Colour.purple(),
 }
 
 # Input colour as either a hex code or a name string
