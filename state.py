caa2efa6b6af49556e491fbadc14c5d6989e557b
Kurtis Knodel <kurtisknodel@gmail.com>
2021-03-09 17:31:14 -0800

    Added data saving and loading

--- state.py
+++ state.py
@@ -1,16 +1,22 @@
+import savedata
+
 DEFAULT_PREFIX = "$"
+PREFIX_NAME = "prefixes"
 
 class State:
 	def __init__(self, client):
 		self.client = client
-		self.prefixes = {} # TODO: Load prefixes from save file
+		self.prefixes = savedata.load_dict(PREFIX_NAME)
 	
+	#NOTE: Ensure all keys are strings when getting and setting dictionary data. This is for JSON compatibility.
 	def set_prefix(self, guild, prefix):
-		self.prefixes[guild] = prefix
-		# TODO: Save prefixes
+		key = str(guild.id)
+		self.prefixes[key] = prefix
+		savedata.save_dict(PREFIX_NAME, self.prefixes)
 	
 	def get_prefix(self, guild):
 		try:
-			return self.prefixes[guild]
+			key = str(guild.id)
+			return self.prefixes[key]
 		except KeyError:
 			return DEFAULT_PREFIX
